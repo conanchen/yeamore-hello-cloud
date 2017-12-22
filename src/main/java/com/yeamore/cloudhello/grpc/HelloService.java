ce6b700e3e8dcbc103f0f21e42f6fc1a6d149c99
conanchen <conan8chan@yahoo.com>
2017-12-22 16:30:35 +0800

    first commit

--- src/main/java/com/yeamore/cloudhello/grpc/HelloService.java
+++ src/main/java/com/yeamore/cloudhello/grpc/HelloService.java
@@ -9,17 +9,22 @@ import org.lognet.springboot.grpc.GRpcService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+
 @GRpcService(interceptors = {LogInterceptor.class})
 public class HelloService extends HelloGrpc.HelloImplBase {
     private static final Logger log = LoggerFactory.getLogger(HelloService.class);
     private static final Gson gson = new Gson();
+    private static final SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+    private static final Calendar calendar = Calendar.getInstance();
 
     @Override
     public void sayHello(HelloRequest request, StreamObserver<HelloReply> responseObserver) {
-        final HelloReply.Builder replyBuilder = HelloReply.newBuilder().setMessage("Hello " + request.getName());
+        final HelloReply.Builder replyBuilder = HelloReply.newBuilder().setId(calendar.getTimeInMillis()).setMessage("Hello " + request.getName() + dateFormat.format(calendar.getTime()));
         HelloReply helloReply = replyBuilder.build();
         responseObserver.onNext(helloReply);
-        log.info(String.format("HelloService.sayHello() : %s", gson.toJson(helloReply)));
+        log.info(String.format("HelloService.sayHello() %d:%s gson=%s", helloReply.getId(), helloReply.getMessage(), gson.toJson(helloReply)));
         responseObserver.onCompleted();
     }
 }
